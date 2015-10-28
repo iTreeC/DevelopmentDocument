ec44fba4fb554eddd4c1b4aaa53de7586d2444b8
iTreezp <563545279@qq.com>
2015-10-28 15:44:25 +0800

    更改iTreebackend
    删除原有的并上传新的

--- iTreeWeb/iTreebackend/com/itree/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.itree.dao;
-
-import java.util.List;
-
-import com.itree.entity.User;
-
-public interface UserDao {
-	public List<User> findAll();
-}
--- iTreeWeb/iTreebackend/com/itree/utils/hibernateUtils.java
+++
@@ -1,6 +0,0 @@
-package com.itree.utils;
-
-public class hibernateUtils {
-	
-
-}
